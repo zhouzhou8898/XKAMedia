44067ad7beeac72d0316533e46e397ec40a8d932
zhouzhou8898 <yuhanzhou0722722@outlook.com>
2024-08-17 17:18:39 +0800

    v1

---
+++ src/components/Spline.tsx
@@ -0,0 +1,9 @@
+import Spline from '@splinetool/react-spline/next';
+
+export default function Home() {
+  return (
+    <main className=' h-screen'>
+      <Spline scene='https://prod.spline.design/dvyxjgf2JGtIcLpF/scene.splinecode' />
+    </main>
+  );
+}
